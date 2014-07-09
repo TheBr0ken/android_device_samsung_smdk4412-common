55b3205fabf33e3f76a43ab57a51762a4eec2bb9
Darescu Ionut <ionut.darescu@gmail.com>
2014-07-09 07:16:08 -0400

    Add Device settings

--- common.mk
+++ common.mk
@@ -63,6 +63,7 @@ PRODUCT_COPY_FILES += \
 # Packages
 PRODUCT_PACKAGES := \
     AdvancedDisplay \
+    DeviceSettings \
     audio.a2dp.default \
     audio.primary.smdk4x12 \
     audio.r_submix.default \
